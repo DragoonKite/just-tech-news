66eaa4eb64a83bc622589a2863b6fe649642b186
DragoonKite <frank.depaolo@yahoo.com>
2021-03-22 17:51:13 -0500

    added login/logout functionality as well as cookies to track user

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {Post, User, Comment} = require('../models');
+const { route } = require('./api');
 
 router.get('/', (req,res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: [
             'id', 'post_url', 'title', 'created_at',
@@ -31,6 +34,15 @@ router.get('/', (req,res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.get('/login', (req,res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+  res.render('login');
 });
 
 module.exports = router;
